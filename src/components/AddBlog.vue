c3aa66a38d10888780e166fb9a8a0525597742d6
devang <devang.patil@upgrad.com>
2019-04-20 13:18:34 +0530

    attach select

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -16,6 +16,10 @@
             <label>Wizards</label>
             <input type="checkbox" value="Wizards" v-model="blog.categories"/>
         </div>
+        <label for="">Author</label>
+        <select name="" id="" v-model="blog.author">
+            <option v-for="author in authors">{{author}}</option>
+        </select>
     </form>
     <div id="preview">
         <h3>Preview Blog</h3>
@@ -25,6 +29,7 @@
         <ul>
             <li v-for="category in blog.categories">{{ category }}</li>
         </ul>
+        <p>Author: {{blog.author}}</p>
     </div>
   </div>
 </template>
@@ -37,8 +42,10 @@ export default {
         blog:{
             title:"",
             content:"",
-            categories:[]
-        }
+            categories:[],
+            author:""
+        },
+        authors: ["Devang", "Virat", "Avenger", "John"]
     }
   },
   methods: {
