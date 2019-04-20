6bdb722e55dbaee90aef649bcce497160313814b
devang <devang.patil@upgrad.com>
2019-04-20 15:50:31 +0530

    local directive and filter

--- src/components/showBlog.vue
+++ src/components/showBlog.vue
@@ -3,7 +3,7 @@
         <h1>All Blogs</h1>
         <input type="text" v-model="search" placeholder="Searchbox" />
         <div v-for="(blog,index) in filteredBlogs" class="single-blog" v-bind:key="index">
-            <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+            <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
             <article>{{blog.body | snippet }}</article>
         </div>
     </div>    
@@ -27,6 +27,34 @@ export default {
             return this.blogs.filter((blog) => {
                 return blog.title.match(this.search)
             });
+        }
+    },
+    filters: {
+        toUppercase(value){
+             return value.toUpperCase();
+        },
+        snippet(value){
+            return value.slice(0,100)+'...';
+        }
+    },
+    directives:{
+        'rainbow':{
+            bind(el, binding, vnode){
+                el.style.color = "#" + Math.random().toString().slice(2,8);
+            },
+        },
+        'theme':{
+              bind(el, binding, vnode){
+                if(binding.value == 'wide'){
+                el.style.maxWidth = "1200px"
+                }else if(binding.value== 'narrow'){
+                el.style.maxWidth = '500px'
+                }
+                if(binding.arg == 'column'){
+                el.style.background= '#ddd';
+                el.style.padding= '20px';
+                }
+            }
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -4,35 +4,28 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-// custom directive
-Vue.directive("rainbow", {
-  bind(el, binding, vnode){
-    el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
-});
+// // custom directive
+// Vue.directive("rainbow", {
 
-Vue.directive("theme", {
-  bind(el, binding, vnode){
-    if(binding.value == 'wide'){
-      el.style.maxWidth = "1200px"
-    }else if(binding.value== 'narrow'){
-      el.style.maxWidth = '500px'
-    }
-    if(binding.arg == 'column'){
-      el.style.background= '#ddd';
-      el.style.padding= '20px';
-    }
-  }
-});
+// });
 
-// filter
-Vue.filter('to-uppercase', function(value){
-  return value.toUpperCase();
-})
+// Vue.directive("theme", {
+//   bind(el, binding, vnode){
+//     if(binding.value == 'wide'){
+//       el.style.maxWidth = "1200px"
+//     }else if(binding.value== 'narrow'){
+//       el.style.maxWidth = '500px'
+//     }
+//     if(binding.arg == 'column'){
+//       el.style.background= '#ddd';
+//       el.style.padding= '20px';
+//     }
+//   }
+// });
 
-Vue.filter('snippet', function(value){
-  return value.slice(0,100)+'...';
-})
+// Vue.filter('snippet', function(value){
+//   return value.slice(0,100)+'...';
+// })
 
 new Vue({
   el: '#app',
